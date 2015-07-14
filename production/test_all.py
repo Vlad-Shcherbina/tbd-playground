2f5a6aedfd5470f1d8efb1762eff3443dc8a4888
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2015-07-14 23:24:35 +0200

    exclude by absolute path

--- production/test_all.py
+++ production/test_all.py
@@ -2,11 +2,17 @@ import sys, os
 
 import nose
 
+from production import utils
+
 
 if __name__ == '__main__':
     argv = sys.argv + [
         '--verbose', '--with-doctest',
         '--logging-level=DEBUG',
-        '--exclude-dir=swig_demo',
+
+        # Othersie it fails, probably because of some weird interaction between
+        # distutils argv and nose argv.
+        '--exclude-dir=' + os.path.join(
+            utils.get_project_root(), 'production', 'swig_demo'),
         ]
     nose.run_exit(argv=argv)
--- production/utils.py
+++ production/utils.py
@@ -1,9 +1,13 @@
 import os
 
 
-project_root = os.path.normpath(
+_project_root = os.path.normpath(
     os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
 
+def get_project_root():
+    return _project_root
+
+
 def get_data_dir():
-    return os.path.join(project_root, 'data')
+    return os.path.join(_project_root, 'data')
