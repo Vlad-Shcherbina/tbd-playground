39bd8029f9c817455c55ac5b28d39fdf80976d11
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2015-07-28 20:03:01 +0200

    add scratch; limit test runs to production package
    (cherry picked from commit 3159f1cd24a66e5215673a03db7ccdcaf651076c)

--- production/test_all_with_coverage.py
+++ production/test_all_with_coverage.py
@@ -5,20 +5,18 @@ import coverage.cmdline
 
 
 if __name__ == '__main__':
+    argv = sys.argv + [
+        'production',
+        '--verbose', '--with-doctest',
+        '--logging-level=DEBUG',
+        '--with-coverage', '--cover-branches', '--cover-erase',
+        '--cover-package=production',
+        ]
+    nose.main(argv=argv, exit=False)
 
     cur_dir = os.getcwd()
     try:
         os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-        argv = sys.argv + [
-            '--verbose', '--with-doctest',
-            '--logging-level=DEBUG',
-            '--with-coverage', '--cover-branches', '--cover-erase',
-            '--cover-package=production',
-            ]
-        nose.main(argv=argv, exit=False)
-
         coverage.cmdline.main(argv=['html'])
-
     finally:
         os.chdir(cur_dir)
