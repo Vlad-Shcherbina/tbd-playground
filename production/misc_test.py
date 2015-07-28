a3c290686d71893cb57b177d5039977068fe06e5
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2015-07-28 21:16:18 +0200

    MISC as swigged c++ extension

--- production/misc_test.py
+++ production/misc_test.py
@@ -1,22 +1,41 @@
 import sys
+import unittest
 
 import nose
 from nose.tools import eq_
 
 from production.naive_misc import NaiveMisc
+from production.cpp_misc.cpp_misc import CppMisc
 
 
-def test_minimal():
-    m = NaiveMisc(word_size=2)
-    m.set_word(0, 1)
-    m.set_word(1, 0)
-    m.set_word(2, 3)
-    m.set_word(3, 2)
-    eq_(str(m), 'NaiveMisc(ip=0, memory=[1, 0, 3, 2])')
+def canonical_str(m):
+    assert m.__class__.__name__.endswith('Misc')
+    return str(m).replace(m.__class__.__name__, '*Misc')
 
-    m.step()
 
-    eq_(str(m), 'NaiveMisc(ip=0, memory=[1, 2, 3, 2])')
+class CommonMiscTests(object):
+
+    Machine = None
+
+    def test_minimal(self):
+        m = self.Machine(2)
+        m.set_word(0, 1)
+        m.set_word(1, 0)
+        m.set_word(2, 3)
+        m.set_word(3, 2)
+        eq_(canonical_str(m), '*Misc(ip=0, memory=[1, 0, 3, 2])')
+
+        m.step()
+
+        eq_(canonical_str(m), '*Misc(ip=0, memory=[1, 2, 3, 2])')
+
+
+class NaiveMiscTests(unittest.TestCase, CommonMiscTests):
+    Machine = NaiveMisc
+
+
+class CppMiscTests(unittest.TestCase, CommonMiscTests):
+    Machine = CppMisc
 
 
 if __name__ == '__main__':
